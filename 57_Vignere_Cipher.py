8f87dfde9b2681f15ae730633325c3230a7cb6b5
NANODESU <nanod3su@gmail.com>
2017-12-16 11:02:06 +0700

    add encryption with low-case chars

--- 57_Vignere_Cipher.py
+++ 57_Vignere_Cipher.py
@@ -1,1 +1,13 @@
- 
+s = str(input("String: "))
+key = str(input("Key: "))
+enc = ''
+i = 0
+
+for i in range(len(s)):
+    k = ord(s[i]) + ord(key[i%len(key)])%97
+    if (k < 123):
+        enc += chr(k)
+    else:
+        enc += chr(k-26)
+
+print(enc)
